971b90960f4b05dbff394d47c3f635d01336ee01
asadiceccarelli <a.ceccarelli@hotmail.co.uk>
2022-08-22 21:04:38 +0100

    Further develop pipeline.

--- project/eda.py
+++ project/eda.py
@@ -7,11 +7,16 @@ import plotly.express as px
 logging.basicConfig(level=logging.INFO)
 
 
-def concatenate_data():
-    """Combine all dataframes into one singular dataframe, main_df. Saved as a .csv file."""
-    global main_df
+def concatenate_data(dataset_path, elo_path):
+    """Combine all DataFrames into one singular DataFrame.
+    Args:
+        dataset_path (str): Relative path of .csv datasets.
+        pickle_path (str): Relative path of .pkl file containing
+            elo data.
+    Returns:
+        main_df (DataFrame): Contains a concatenation of all data."""
     logging.info('Concatenating data...')
-    paths = glob.glob('Football-Dataset/*/*')
+    paths = glob.glob(dataset_path)
     pd_list = []
     for path in paths:
         temp_csv = pd.read_csv(path)
@@ -25,78 +30,92 @@ def concatenate_data():
         return new_link_complete
     
     raw_df['clean_link'] = raw_df.Link.apply(clean_link)
-    match_info_df = pd.read_csv('https://aicore-files.s3.amazonaws.com/Data-Science/Match_Info.csv')
-    team_info_df = pd.read_csv("https://aicore-files.s3.amazonaws.com/Data-Science/Team_Info.csv")
-    elo_dict = pickle.load(open('elo_dict.pkl', 'rb'))
+    match_info_df = pd.read_csv('Additional-Data/match_info.csv', index_col=[0])
+    team_info_df = pd.read_csv('Additional-Data/team_info.csv', index_col=[0])
+    elo_dict = pickle.load(open(elo_path, 'rb'))  # Read binary file
     elo_df = pd.DataFrame.from_dict(elo_dict)
     elo_df = elo_df.transpose().reset_index().rename(columns={'index': 'Link'})
 
     match_df = pd.merge(raw_df, match_info_df, left_on='clean_link', right_on='Link')
-    mach_elo_df = pd.merge(match_df, elo_df, left_on='Link_x', right_on='Link', how='left')
-    main_df = pd.merge(mach_elo_df, team_info_df, left_on='Home_Team', right_on='Team', how='left')
-
-    main_df.to_csv('project/main_df')
-
+    match_elo_df = pd.merge(match_df, elo_df, left_on='Link_x', right_on='Link', how='left')
+    main_df = pd.merge(match_elo_df, team_info_df, left_on='Home_Team', right_on='Team', how='left')
+    return main_df
 
 
-def clean_data():
+def clean_data(df):
     """Further cleans the dataframe by dropping unnecessary columns,
     removing duplicates and cleaning referee and capacity data.
+    Args:
+        df (DataFrame): DataFrame to be cleaned.
+    Returns:
+        df (DataFrame): Clean DataFrame.
     """
     logging.info('Cleaning data...')
-    main_df.columns= main_df.columns.str.lower()
-    main_df.drop(['link_x', 'link_y', 'team'], axis=1, inplace=True)
-    main_df.drop_duplicates('clean_link', inplace=True)
-    main_df.drop(main_df[main_df.league == 'eerste_divisie'].index, inplace=True)
-    main_df.drop(main_df[main_df.league == 'segunda_liga'].index, inplace=True)
-    main_df.drop(main_df[(main_df.league == 'championship') & (main_df['season'] == 1998)].index, inplace=True)  # Drop incomplete 1998 championship data
-    main_df['referee'] = main_df.referee.apply(lambda x: x.split('\r\n')[1][9:] if type(x)==str else x)
-    main_df['capacity'] = main_df.capacity.apply(lambda x: int(x.replace(',', '')) if type(x)==str else x)
+    df.columns = df.columns.str.lower()
+    df.drop(['link_x', 'link_y', 'team'], axis=1, inplace=True)
+    df.drop_duplicates('clean_link', inplace=True)
+    df.drop(df[df.league == 'eerste_divisie'].index, inplace=True)
+    df.drop(df[df.league == 'segunda_liga'].index, inplace=True)
+    df.drop(df[(df.league == 'championship') & (df['season'] == 1998)].index, inplace=True)  # Drop incomplete data
+    df['referee'] = df.referee.apply(lambda x: x.split('\r\n')[1][9:] if type(x)==str else x)
+    df['capacity'] = df.capacity.apply(lambda x: int(x.replace(',', '')) if type(x)==str else x)
+    return df
 
 
-def create_outcome():
+def create_outcome(df):
     """Creates an outcome column where a home win == 1, draw == 0 and
     away win == -1. Drops uneccessary link and result columns.
+    Args:
+        df (DataFrame): DataFrame without outcome column.
+    Returns:
+        df (DataFrame): DataFrame with outcome calculated.
     """
-    main_df.drop(main_df[main_df.result.str.len() != 3].index, inplace=True)  # Remove any scores set as dates
-    main_df['home_goals'] = main_df.result.apply(lambda x: x.split('-')[0])
-    main_df['away_goals'] = main_df.result.apply(lambda x: x.split('-')[-1])
-    main_df['home_goals'] = main_df.home_goals.astype('int64')
-    main_df['away_goals'] = main_df.away_goals.astype('int64')
-    main_df.drop('result', axis=1, inplace=True)
-    main_df['outcome'] = main_df[['home_goals', 'away_goals']].apply(
+    df.drop(df[df.result.str.len() != 3].index, inplace=True)  # Remove any scores incorrectly formatted
+    df['home_goals'] = df.result.apply(lambda x: x.split('-')[0])
+    df['away_goals'] = df.result.apply(lambda x: x.split('-')[-1])
+    df['home_goals'] = df.home_goals.astype('int64')
+    df['away_goals'] = df.away_goals.astype('int64')
+    df.drop('result', axis=1, inplace=True)
+    df['outcome'] = df[['home_goals', 'away_goals']].apply(
         lambda x: 1 if x['home_goals'] > x['away_goals'] else (-1 if x['home_goals'] < x['away_goals'] else 0), axis=1)
+    return df
 
 
-def plot_outcome():
+def plot_outcome(df):
     """Plots the outcome of all leagues against time and saves
     the figure to README-images.
+    Args:
+        df (DataFrame): DataFrame containing all data.
     """
-    outcome_line = px.line(main_df.groupby('season')['outcome'].mean(), title='Outcome of matches over time')
+    outcome_line = px.line(df.groupby('season')['outcome'].mean(), title='Outcome of matches over time')
     outcome_line.update_layout(showlegend=False)
     outcome_line.write_image('README-images/outcome-over-time.png')
 
 
-def plot_goals():
+def plot_goals(df):
     """ Plots the average goals per game of each league and saves
     the figure to README-images.
+    Args:
+        df (DataFrame): DataFrame containing all data.
     """
-    main_df['home_goals'] = main_df.home_goals.astype('int64')
-    main_df['away_goals'] = main_df.away_goals.astype('int64')
-    main_df['total_goals'] = main_df[['home_goals','away_goals']].apply(lambda x: x['home_goals'] + x['away_goals'], axis=1)
-    goals_bar = px.bar(main_df.groupby('league')['total_goals'].mean(), title='Average number of goals per game')
+    df['home_goals'] = df.home_goals.astype('int64')
+    df['away_goals'] = df.away_goals.astype('int64')
+    df['total_goals'] = df[['home_goals','away_goals']].apply(lambda x: x['home_goals'] + x['away_goals'], axis=1)
+    goals_bar = px.bar(df.groupby('league')['total_goals'].mean(), title='Average number of goals per game')
     goals_bar.update_layout(showlegend=False)
     goals_bar.write_image('README-images/average_goals.png')
 
 
-def plot_rounds():
+def plot_rounds(df):
     """Plots a line graph of the number of rounds in each league
     against time and saves the figure to README-images.
+    Args:
+        df (DataFrame): DataFrame containing all data.
     """
     dict = {}
-    leagues = main_df['league'].unique()
+    leagues = df['league'].unique()
     for league in leagues:
-        dict[league] = main_df[main_df.league == f'{league}'].groupby('season')['round'].max()
+        dict[league] = df[df.league == f'{league}'].groupby('season')['round'].max()
     rounds_line = px.line(
         dict, labels={'value': 'no. rounds'},
         title='Rounds per season for each league',
@@ -104,12 +123,14 @@ def plot_rounds():
         )
     rounds_line.write_image('README-images/rounds.png')
 
-def plot_capacity():
+def plot_capacity(df):
     """Plots a scatter chart of stadium size vs the average home
     outcome of each game and saves the figure to README-images.
+    Args:
+        df (DataFrame): DataFrame containing all data.
     """
-    home_outcome = main_df.groupby('home_team')['outcome'].mean()
-    capcity_outcome = pd.merge(home_outcome, main_df, left_on='home_team', right_on='home_team', how='left')
+    home_outcome = df.groupby('home_team')['outcome'].mean()
+    capcity_outcome = pd.merge(home_outcome, df, left_on='home_team', right_on='home_team', how='left')
     capcity_outcome_scatter = px.scatter(
         x=capcity_outcome['capacity'],
         y=capcity_outcome['outcome_x'],
@@ -121,10 +142,15 @@ def plot_capacity():
     capcity_outcome_scatter.write_image('README-images/capacity.png')
 
 
-def plot_cards():
-    main_df['home_cards'] = main_df.apply(lambda x: x['home_yellow'] + x['home_red'], axis=1)
-    home_cards = main_df.groupby('home_team')['home_cards'].mean()
-    capacity_cards = pd.merge(home_cards, main_df, left_on='home_team', right_on='home_team', how='left')
+def plot_cards(df):
+    """Plots a scatter chart of number of cards vs the average home
+    outcome of each game and saves the figure to README-images.
+    Args:
+        df (DataFrame): DataFrame containing all data.
+    """
+    df['home_cards'] = df.apply(lambda x: x['home_yellow'] + x['home_red'], axis=1)
+    home_cards = df.groupby('home_team')['home_cards'].mean()
+    capacity_cards = pd.merge(home_cards, df, left_on='home_team', right_on='home_team', how='left')
     capacity_cards
     capacity_cards_scatter = px.scatter(
         x=capacity_cards['capacity'],
@@ -137,11 +163,25 @@ def plot_cards():
     capacity_cards_scatter.write_image('README-images/cards.png')
 
 
+def perform_eda(dataset_path, pickle_path):
+    """Concatenates, cleans and creates a column containing the match outcomes.
+    Args:
+        dataset_path (str): Relative path of .csv datasets.
+        pickle_path (str): Relative path of .pkl file containing
+            elo data.
+    Returns:
+        main_df_final (DataFrame): Cleaned DataFrame with all columns.
+    """
+    main_df = concatenate_data(dataset_path, pickle_path)
+    main_df_clean = clean_data(main_df)
+    main_df_final = create_outcome(main_df_clean)
+    return main_df_final
+
+
 if __name__ == '__main__':
-    concatenate_data()
-    clean_data()
-    # plot_rounds()
-    create_outcome()
-    plot_outcome()
-    plot_goals()
-    plot_capacity()
+    perform_eda('Football-Dataset/*/*', 'Additional-Data/elo_dict.pkl', 'original')
+    main_df = pd.read_csv('project/dataframes/main_df.csv')
+    # # plot_rounds(main_df)
+    # plot_outcome(main_df)
+    # plot_goals(main_df)
+    # plot_capacity(main_df)
--- project/rds.py
+++ project/rds.py
@@ -1,3 +1,4 @@
+from distutils.command.upload import upload
 import os
 import logging
 import pandas as pd
@@ -9,6 +10,9 @@ load_dotenv()
 
 
 def rds_connect():
+    """Connects to the AWS database using environment variables
+    in a .env file.
+    """
     DATABASE_TYPE = 'postgresql'
     DBAPI = 'psycopg2'
     HOST = os.environ['DB_HOST']
@@ -19,11 +23,20 @@ def rds_connect():
     return create_engine(f"{DATABASE_TYPE}+{DBAPI}://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}")
 
 
-logging.info('Connecting to RDS...')
-engine = rds_connect()
+def upload_initial_data():
+    """Uploads the initial dataframe into an RDS database in the cloud."""
+    logging.info('Connecting to RDS...')
+    engine = rds_connect()
+    logging.info('Uploading initial dataset to RDS...')
+    clean_dataset = pd.read_csv('project/dataframes/cleaned_dataset.csv', index_col=0)
+    df_name = 'match-results'
+    clean_dataset.to_sql(df_name, engine, if_exists='replace', index=False)
 
-logging.info('Uploading to RDS...')
-clean_dataset = pd.read_csv('project/dataframes/cleaned_dataset.csv')
-df_name = 'match-results'
-clean_dataset.to_sql(df_name, engine, if_exists='replace', index=False)
 
+def upload_additional_data():
+    logging.info('Connecting to RDS...')
+    engine = rds_connect()
+    logging.info('Uploading additional dataset to RDS...')
+    clean_dataset_additional = pd.read_csv('project/dataframes/cleaned_dataset.csv', index_col=0)
+    df_name = 'match-results'
+    clean_dataset_additional.to_sql(df_name, engine, if_exists='append', index=False)
