f065f530d0a1a61dcc3a15528c94940731d4faf8
asadiceccarelli <a.ceccarelli@hotmail.co.uk>
2022-08-25 21:37:20 +0100

    Perform feature selection.

--- model/baseline_model.py
+++ model/baseline_model.py
@@ -6,13 +6,13 @@ from sklearn.metrics import accuracy_score
 
 cleaned_dataset = pd.read_csv('preparation/dataframes/cleaned_dataset.csv')
 X = cleaned_dataset[[
-    'home_scored_sofar', 'away_scored_sofar', 'home_conceeded_sofar', 'away_conceeded_sofar', 'home_points_sofar',
-    'away_points_sofar', 'elo_home', 'elo_away', 'home_form', 'away_form']]
+    'round', 'home_scored_sofar', 'away_scored_sofar', 'home_conceeded_sofar', 'away_conceeded_sofar',
+    'home_points_sofar', 'away_points_sofar', 'elo_home', 'elo_away', 'home_form', 'away_form']]
 y = cleaned_dataset['outcome']
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-log_reg = LogisticRegression(multi_class='multinomial', solver='newton-cg')
-dump(log_reg, 'model/baseline.joblib') 
-log_reg.fit(X_train, y_train)
-y_pred = log_reg.predict(X_test)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=13)
+model = LogisticRegression(multi_class='multinomial', solver='newton-cg')
+dump(model, 'model/baseline.joblib') 
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
 print(accuracy_score(y_test, y_pred))
